471a8abfd554cd8c051f1d999356caa9f208db9d
Laura Bassani <bassanilaurarita@gmail.com>
2021-01-18 16:31:21 +0000

    Products: Added products in welcome view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link href="/css/app.css" rel="stylesheet">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
