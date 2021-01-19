63496a04d4edb8fae2a2d8c478cfe8b556e1c68c
Eni <estefanielunardi@users.noreply.github.com>
2021-01-19 16:24:59 +0100

    Catalog: Modified cart buttom position

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,7 +18,7 @@
     <div class="container my-12 mx-auto px-4 md:px-12">
         <div class="flex flex-wrap -mx-1 lg:-mx-4">
 
-    <div class="bg-yellow-400 rounded-full fixed bottom-10 right-10 z-40 shadow-2xl buttomDesktop buttomPhone">
+    <div class="bg-yellow-400 rounded-full fixed bottom-20 right-10 z-40 shadow-2xl buttomDesktop buttomPhone">
         <svg class="text-white w-full text-center p-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
         </svg>
