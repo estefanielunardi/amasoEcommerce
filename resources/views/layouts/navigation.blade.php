3e7bea222c1073f790212188283ae9ca8d87af7c
Joaquim-Frances <quimifrances@gmail.com>
2021-01-19 20:38:55 +0100

    NavBar: Logo Route to Home view

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,7 +5,7 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="flex-shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
+                    <a href="{{ route('home') }}">
                         <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
                     </a>
                 </div>
