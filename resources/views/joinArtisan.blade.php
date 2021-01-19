3deec2b878c4a92d40bb6e94d0de059492ba06cf
Joaquim-Frances <quimifrances@gmail.com>
2021-01-19 15:50:03 +0100

    Artisan Unete: Filled view with html

--- resources/views/joinArtisan.blade.php
+++ resources/views/joinArtisan.blade.php
@@ -1,12 +1,43 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>JOIN US</h1>
-</body>
-</html>
+<x-app-layout>
+    <section>
+        <h1>Únete a nosotros</h1>
+        <h2>¿Quieres formar parte del equipo de productores amasó?</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non consequatur sequi vel maxime magni enim, facere, nisi illum eum aperiam labore amet aliquam. Culpa aliquam, perferendis quam ratione mollitia impedit, ab rerum adipisci officia doloremque, praesentium sed aspernatur facilis nesciunt at corrupti? Pariatur vel quia aperiam ducimus, iste et ullam nobis, deleniti ratione tempora quo a autem excepturi quidem sit voluptatibus sequi. Quibusdam quas quidem iure! Mollitia repellendus veritatis tempore.</p>
+    </section>
+    <section>
+        <form action="">
+            <div>
+                <label for="nombre">Nombre y apellidos.</label>
+                <input type="text" id="nombre">
+            </div>
+            <div>
+                <label for="localidad">Localidad.</label>
+                <input type="text" id="localidad">
+            </div>
+            <div>
+                <label for="correo">Email.</label>
+                <input type="text" id="correo">
+            </div>
+            <div>
+                <label for="contrseña">Contraseña.</label>
+                <input type="text" id="contraseña">
+            </div>
+            <div>
+                <label for="sobreTi">Cunetanos algo sobre ti.</label>
+                <input type="text" id="sobreTi">
+            </div>
+            <div>
+                <button>Añade tu certificado.</button>
+            </div>
+            <div>
+                <button type="submit">ENVIAR</button>
+            </div>
+        </form>
+    </section>
+
+
+
+
+
+
+</x-app-layout>
