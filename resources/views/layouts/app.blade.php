e3bb8b1052f163f5dac5cb1624a3056be61b0545
Carmenprz <carmenp.gomez99@gmail.com>
2021-01-20 12:46:59 +0100

    Style: changed background to white from app blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+        <div class="min-h-screen bg-white">
             @include('layouts.navigation')
 
             <!-- Page Content -->
