49009f2b9e898f8b913f32b8e71ce88698ea5e42
Eni <estefanielunardi@users.noreply.github.com>
2021-01-20 15:08:28 +0100

    Create product: Added create and store routeand form

--- routes/web.php
+++ routes/web.php
@@ -11,13 +11,23 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard'); 
 
+Route::get('/product/create', function () {
+    return view('products.create');
+})->middleware(['auth'])->name('newProduct'); 
+
 
 require __DIR__.'/auth.php';
 
 Route::get('/', [App\Http\Controllers\ProductController::class, 'getProducts'])->name('home');
+
+Route::post('/product/store', [App\Http\Controllers\ProductController::class, 'store'])->name('storeProduct');
+
 
 Route::get('/artisan/{id}',  [\App\Http\Controllers\ArtisanController::class, 'profile'])->name('artisanProfile');
 
 Route::get('/joinArtisan', [App\Http\Controllers\ArtisanController::class, 'joinUs'])->name('joinArtisan');
 
 Route::post('/joinArtisan', [App\Http\Controllers\ArtisanController::class, 'store'])->name('artisanStore');
+
+
+
