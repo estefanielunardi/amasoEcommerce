b0b91d7f0559e578259cd50de4414d89aa76bd08
Joaquim <quimifrances@gmail.com>
2021-01-21 22:15:34 +0100

    NavBar: Adjusted logo, added background image.

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -4,7 +4,7 @@
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
-                <div class="flex-shrink-0 flex items-center">
+                <div class="flex-shrink-0 flex items-center ml-6">
                     <a href="{{ route('home') }}">
                         <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
                     </a>
