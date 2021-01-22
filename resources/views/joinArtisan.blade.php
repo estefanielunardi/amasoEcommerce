1bf0f0379c1aa8763ebcfc29b09602209e1ba8dd
Carmenprz <carmenp.gomez99@gmail.com>
2021-01-22 10:16:58 +0100

    Profile artisan: style form register

--- resources/views/joinArtisan.blade.php
+++ resources/views/joinArtisan.blade.php
@@ -6,26 +6,31 @@
     </section>
     <section class="flex flex-col m-10">
         <div class="max-w-4xl">
-            <form action="">
+            <form action="" >
+            
                 <div class="flex flex-col my-4 text-xl greenAmaso">
                     <label for="nombre" class="font-serif">Nombre y apellidos.</label>
-                    <input type="text" id="nombre" class="w-100 border-solid border-2 borderGreen rounded shadow-md h-10">
+                    <input type="text" id="name" name="name" class="w-100 border-solid border-2 borderGreen rounded shadow-md h-10">
+                </div>
+                <div class="flex flex-col my-4 text-xl greenAmaso">
+                    <label for="image" class="font-serif">Imagen de perfil.</label>
+                    <input type="text" id="image" name="image" class="w-100 border-solid border-2 borderGreen rounded shadow-md h-10">
                 </div>
                 <div class="flex flex-col my-4 text-xl greenAmaso">
                     <label for="localidad" class="font-serif">Localidad.</label>
-                    <input type="text" id="localidad" class="w-100 border-solid border-2 borderGreen rounded shadow-md h-10">
+                    <input type="text" id="location" name="location" class="w-100 border-solid border-2 borderGreen rounded shadow-md h-10">
                 </div>
                 <div class="flex flex-col my-4 text-xl greenAmaso">
                     <label for="correo" class="font-serif">Email.</label>
-                    <input type="text" id="correo" class="w-100 border-solid border-2 borderGreen rounded shadow-md h-10">
+                    <input type="text" id="email" name="email" class="w-100 border-solid border-2 borderGreen rounded shadow-md h-10">
                 </div>
                 <div class="flex flex-col my-4 text-xl greenAmaso">
                     <label for="contrseña" class="font-serif">Contraseña.</label>
-                    <input type="text" id="contraseña" class="w-100 border-solid border-2 borderGreen rounded shadow-md h-10">
+                    <input type="text" id="password" name="password" class="w-100 border-solid border-2 borderGreen rounded shadow-md h-10">
                 </div>
                 <div class="flex flex-col my-4 text-xl greenAmaso">
-                    <label for="sobreTi" class="font-serif">Cuéntanos algo sobre ti.</label>
-                    <textarea rows="5" cols="80" id="sobreTi" class="w-100 border-solid border-2 borderGreen rounded shadow-md">
+                    <label for="description" class="font-serif">Cuéntanos algo sobre ti.</label>
+                    <textarea rows="5" cols="80" id="description" name="description" class="w-100 border-solid border-2 borderGreen rounded shadow-md">
                     </textarea>
                 </div>
                 <div class="flex justify-end">
