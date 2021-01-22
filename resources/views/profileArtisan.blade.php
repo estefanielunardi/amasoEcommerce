fe7a0518e029b5f42d1b9a45150ba9fb5d9ee0db
Laura Bassani <bassanilaurarita@gmail.com>
2021-01-22 16:35:02 +0000

    Artisan profile: hide edit profile button for no auth user

--- resources/views/profileArtisan.blade.php
+++ resources/views/profileArtisan.blade.php
@@ -1,15 +1,14 @@
 <x-app-layout>
     <body>
         <section class="block space-y-8 ... p-12 ">
-        <button class= "greenLightBg flex flex-row align-start text-sm text-white mt-4 px-6 py-2  rounded-xl shadow-md">
-               
+        @auth
+        <button class= "greenLightBg flex flex-row align-start text-sm text-white mt-4 px-6 py-2  rounded-xl shadow-md">               
                     <svg  width="24" height="24" viewBox="0 0 31 32" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M21.0582 5.7376C21.3442 5.43197 21.6862 5.18819 22.0644 5.02048C22.4427 4.85277 22.8494 4.76449 23.2611 4.7608C23.6727 4.75711 24.0809 4.83808 24.4619 4.99897C24.8428 5.15987 25.189 5.39748 25.48 5.69794C25.7711 5.9984 26.0013 6.35568 26.1571 6.74895C26.313 7.14222 26.3915 7.56359 26.3879 7.98849C26.3843 8.41338 26.2988 8.83329 26.1363 9.2237C25.9739 9.61411 25.7377 9.96721 25.4416 10.2624L24.2125 11.5312L19.8291 7.0064L21.0582 5.7376ZM17.6374 9.2688L4.6499 22.6752V27.2H9.0333L22.0223 13.7936L17.6358 9.2688H17.6374Z" fill="white"/>
-                    </svg>
-              
-            editar la información de mi perfil</button>
-           
-
+                    </svg>              
+            Editar la información de mi perfil
+        </button>
+        @endauth
             <article class="flex justify-start">
                 <div>
                     <img class=" max-w-xs w-174 rounded-xl" src="{{$artisan->image}}" alt="foto julian campesino">
@@ -76,23 +75,22 @@
                         <div class="px-4 py-4 md:px-10">
                             <p class="py-4"> {{$product->price}} €</p>
                             <div class="relative">
-                            @if ($product->stock < $product->sold)
-                                <div class="custom-number-input h-10 w-32 absolute bottom-2 right-1">
-                                    <div class="flex flex-row h-10 w-full rounded-lg relative bg-transparent mt-1">
-                                        <button data-action="decrement" class="counter greenLightBg beigeLight h-full w-20 rounded-l-2xl cursor-pointer outline-none">
-                                            <span class="m-auto text-2xl font-thin">−</span>
-                                        </button>
-                                        <input type="number" class="counter border-transparent outline-none focus:outline-none text-center w-12 greenAmasoBg font-semibold text-md   md:text-basecursor-default flex items-center text-white  outline-none" name="custom-input-number" value="0"></input>
-                                        <button data-action="increment" class="counter  greenLightBg  beigeLight  h-full w-20 rounded-r-2xl cursor-pointer outline-none">
-                                            <span class="m-auto text-2xl font-thin">+</span>
-                                        </button>
+                                @if ($product->stock < $product->sold)
+                                    <div class="custom-number-input h-10 w-32 absolute bottom-2 right-1">
+                                        <div class="flex flex-row h-10 w-full rounded-lg relative bg-transparent mt-1">
+                                            <button data-action="decrement" class="counter greenLightBg beigeLight h-full w-20 rounded-l-2xl cursor-pointer outline-none">
+                                                <span class="m-auto text-2xl font-thin">−</span>
+                                            </button>
+                                            <input type="number" class="counter border-transparent outline-none focus:outline-none text-center w-12 greenAmasoBg font-semibold text-md   md:text-basecursor-default flex items-center text-white  outline-none" name="custom-input-number" value="0"></input>
+                                            <button data-action="increment" class="counter  greenLightBg  beigeLight  h-full w-20 rounded-r-2xl cursor-pointer outline-none">
+                                                <span class="m-auto text-2xl font-thin">+</span>
+                                            </button>
+                                        </div>
                                     </div>
-                                </div>
-                            @else
-                                <p class="greenAmaso">Sold Out</p>
-                            @endif
-
-                        </div>
+                                @else
+                                    <p class="greenAmaso">Sold Out</p>
+                                @endif
+                            </div>
                         </div>
                         @auth
                         <div class="relative h-0 w-32 ..."> 
