61a5fbcccf1a7078ba6a1bbb0f37efbb43f8e3f4
Laura Bassani <bassanilaurarita@gmail.com>
2021-01-23 15:02:28 +0000

    Artisan: stablish relationship with product

--- app/Models/Artisan.php
+++ app/Models/Artisan.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Artisan extends Model
 {
     use HasFactory;
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
