2655c5d9bc9b0e639b03da838b85fc7d086763d6
Laura Bassani <bassanilaurarita@gmail.com>
2021-01-27 15:19:59 +0000

    Refactoring: test change title to camel case

--- tests/Feature/Product/CatalogTest.php
+++ tests/Feature/Product/CatalogTest.php
@@ -2,29 +2,27 @@
 
 namespace Tests\Feature\Product;
 
-use App\Models\Product;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class CatalogTest extends TestCase
 {
     use RefreshDatabase;
-    public function test_route()
+    public function testRouteCatalog()
     {
         $response = $this->get(route('home'));
 
         $response->assertStatus(200);
     }
 
-    public function test_return_catalog_view()
+    public function testReturnCatalogView()
     {
         $response = $this->get(route('home'));
 
         $response->assertViewIs("welcome");
     }
 
-    public function test_catalog_view_has_products()
+    public function testCatalogViewHasProducts()
     {
         $response = $this->get(route('home'));
 
