11416a692172003b79b03f84b8750413333eb588
Joaquim <quimifrances@gmail.com>
2021-01-28 10:40:41 +0100

    Admin: Working on Dashboard functions

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,13 +6,14 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
 use SebastianBergmann\Environment\Console;
 
 class AdminController extends Controller
 {
     public function adminDash(){
 
-        $artisanList = User::where('isArtisan', true)->get();
+        $artisanList = DB::table('artisans')->get();
         return view('adminDashboard', ['artisanList' => $artisanList]);
     }
         
