16b9bf455f8e6d4aaade2c09d7ca343fdd3b04e9
Carmenprz <carmenp.gomez99@gmail.com>
2021-01-28 20:04:06 +0100

    Products: Counter style tailwind changed

--- resources/views/components/counter.blade.php
+++ resources/views/components/counter.blade.php
@@ -1,11 +1,11 @@
 <div>
-        <div class="flex flex-row h-10 w-full rounded-lg relative bg-transparent mt-1">
-                                        <button data-action="decrement" class="counter greenLightBg beigeLight h-full w-20 rounded-l-2xl cursor-pointer outline-none">
-                                            <span class="m-auto text-2xl font-thin">−</span>
+        <div class="flex flex-row h-9 w-full rounded-lg relative bg-transparent mt-1 vollkorn">
+                                        <button data-action="decrement" class="counter greenLightBg beigeLight h-full w-9 rounded-l-2xl cursor-pointer outline-none">
+                                            <span class="m-auto text-2xl font-thin text-white">−</span>
                                         </button>
-                                        <input type="number"  method="post" class="counter border-transparent outline-none focus:outline-none text-center w-12 greenAmasoBg font-semibold text-md   md:text-basecursor-default flex items-center text-white  outline-none" name="custom-input-number" value="0"></input>
-                                        <button data-action="increment" action="{{route('cartAddProduct')}}" class="counter  greenLightBg  beigeLight  h-full w-20 rounded-r-2xl cursor-pointer outline-none">
-                                            <span class="m-auto text-2xl font-thin">+</span>
+                                        <input type="number"  method="post" class="counter border-transparent outline-none focus:outline-none text-center w-10 greenAmasoBg text-xl md:text-basecursor-default flex items-center text-white  outline-none" name="custom-input-number" value="0"></input>
+                                        <button data-action="increment" action="{{route('cartAddProduct')}}" class="counter  greenLightBg  beigeLight  h-full w-9 rounded-r-2xl cursor-pointer outline-none">
+                                            <span class="m-auto text-2xl font-thin text-white">+</span>
                                         </button>
                                     </div>
 
