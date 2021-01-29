a91cdea8c20c9bec151fa699bbbaf117e44a7955
Laura Bassani <bassanilaurarita@gmail.com>
2021-01-29 07:47:14 +0000

    Refactoring: cart

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -1,10 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use App\Models\Product;
 use App\Models\User;
-use Illuminate\Support\Facades\DB;
 
 
 class CartController extends Controller
@@ -12,71 +9,28 @@ class CartController extends Controller
     public function getProducts()
     {
         $id = auth()->id();
-        $products = DB::select("SELECT * FROM `products`
-                WHERE `id` IN (
-                    SELECT `product_id` FROM `product_user` WHERE `user_id` = $id);");
-        
-        $total = 0;
-        foreach($products as $product)
-        {
-            $total += $product->price;
-        }
+        $user = User::find($id);
+        $products = $user->getProductsInBasket($id);
+        $total = $user->calculateTotal($products);
 
         return view('cart', compact("products", "total"));
     }
 
     public function addProduct($product_id)
-        {
-            $user_id = auth()->id();
-            $user = User::find($user_id); 
-            $userProduct = $user->products()->find($product_id);
-            if(is_null($userProduct))
-            {
-                $user->products()->attach($product_id);
-                DB::table('product_user')
-                        ->where('user_id',$user_id)
-                        ->where('product_id',$product_id)
-                        ->increment('amount', 1);
-            }
-            else
-            {
-                DB::table('product_user')
-                        ->where('user_id',$user_id)
-                        ->where('product_id',$product_id)
-                        ->increment('amount', 1);
-            }
+    {
+        $user_id = auth()->id();
+        $user = User::find($user_id); 
+        $user->addProductInCart($product_id, $user_id);
 
+        return back();            
+    }
 
-            return back();            
-        }
-    
     public function removeProduct($product_id)
     {
         $user_id = auth()->id();
         $user = User::find($user_id); 
-        $userProduct = $user->products()->find($product_id);
+        $user->removeProductFromCart($product_id,$user_id);
 
-        if($userProduct)
-        {
-            $amount = DB::table('product_user')
-                        ->where('user_id',$user_id)
-                        ->where('product_id',$product_id)
-                        ->get('amount');
-    
-            $num = $amount[0]->amount;
-            if ($num == 1)
-            {
-                $user->products()->detach($product_id);
-            }
-            else
-            {
-                DB::table('product_user')
-                        ->where('user_id',$user_id)
-                        ->where('product_id',$product_id)
-                        ->decrement('amount', 1);
-            }
-
-        }    
         return back();
     }
 
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,10 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\DB;
 
 class User extends Authenticatable
 {
@@ -46,11 +46,77 @@ class User extends Authenticatable
     {
        return $this->hasOne(Artisan::class);
     }
-
     
     public function products()
     {
         return $this->belongsToMany(Product::class);
+    }
+
+    public function getProductsInBasket($id)
+    {
+        $products = DB::select("SELECT * FROM `products`
+                WHERE `id` IN (
+                    SELECT `product_id` FROM `product_user` WHERE `user_id` = $id);");
+        
+
+        return $products;
+    }
+
+    public function calculateTotal($products)
+    {
+        $total = 0;
+        foreach($products as $product)
+        {
+            $total += $product->price;
+        }
+        return $total;
+    }
+
+    public function addProductInCart($product_id,$user_id)
+    {
+        $userProduct = $this->products()->find($product_id);
+        if(is_null($userProduct))
+        {
+            $this->products()->attach($product_id);
+            DB::table('product_user')
+                    ->where('user_id',$user_id)
+                    ->where('product_id',$product_id)
+                    ->increment('amount', 1);
+        }
+        else
+        {
+            DB::table('product_user')
+                    ->where('user_id',$user_id)
+                    ->where('product_id',$product_id)
+                    ->increment('amount', 1);
+        }
+    }
+
+    public function removeProductFromCart($product_id,$user_id)
+    {
+        $userProduct = $this->products()->find($product_id);
+
+        if($userProduct)
+        {
+            $amount = DB::table('product_user')
+                        ->where('user_id',$user_id)
+                        ->where('product_id',$product_id)
+                        ->get('amount');
+    
+            $num = $amount[0]->amount;
+            if ($num == 1)
+            {
+                $this->products()->detach($product_id);
+            }
+            else
+            {
+                DB::table('product_user')
+                        ->where('user_id',$user_id)
+                        ->where('product_id',$product_id)
+                        ->decrement('amount', 1);
+            }
+
+        }  
     }
 
 }
