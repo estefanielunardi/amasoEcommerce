eeb3c70e7ab64d2e2419305109ad7f37396ecbe5
Laura Bassani <bassanilaurarita@gmail.com>
2021-01-29 07:29:40 +0000

    Artisan: link cart  button with route

--- resources/views/profileArtisan.blade.php
+++ resources/views/profileArtisan.blade.php
@@ -58,11 +58,13 @@
         <div class="container my-12 mx-auto px-4 md:px-12">
             <div class="flex flex-wrap -mx-1 lg:-mx-4">
                 @can('isAuth')
-                <div class="beigeAmasoBg rounded-full fixed bottom-20 right-10 z-40 shadow-2xl buttomDesktop buttomPhone">
-                    <svg class="beigeLight w-full text-center p-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-                    </svg>
-                </div>
+                <x-nav-link :href="route('cart')" :active="request()->routeIs('cart')">
+                    <div class="beigeAmasoBg rounded-full fixed bottom-20 right-10 z-40 shadow-2xl buttomDesktop buttomPhone">
+                        <svg class="beigeLight w-full text-center p-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                        </svg>
+                    </div>
+                </x-nav-link>
                 @endcan
                 @foreach ($products as $product)
                 <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
@@ -90,8 +92,19 @@
                                 @if ($product->stock > $product->sold)
                                     <div class="custom-number-input h-12 w-32 absolute bottom-2 right-1">
                                     <p class="textManifiesto text-base font-bold">Añadir al carrito</p>
-                                       <x-counter></x-counter>
-                                    </div>
+                                    <div class="flex flex-row h-10 w-full rounded-lg relative bg-transparent mt-1">
+                                        <button data-action="decrement" class="counter greenLightBg beigeLight h-full w-20 rounded-l-2xl cursor-pointer outline-none">
+                                        <span class="m-auto text-2xl font-thin">
+                                                <a href="{{ route('removeProductCart' , $product->id) }}">-</a>
+                                        </span>
+                                        </button>
+                                        <input type="number" class="counter border-transparent outline-none focus:outline-none text-center w-12 greenAmasoBg font-semibold text-md   md:text-basecursor-default flex items-center text-white  outline-none" name="custom-input-number" value="0"></input>
+                                        <button data-action="increment" class="counter  greenLightBg  beigeLight  h-full w-20 rounded-r-2xl cursor-pointer outline-none">
+                                            <span class="m-auto text-2xl font-thin">
+                                                <a href="{{ route('cartAddProduct' , $product->id) }}">+</a>
+                                            </span>
+                                        </button>
+                                    </div>      
                                 @else
                                     <p class="greenAmaso">Sold Out</p>
                                 @endif
@@ -133,6 +146,46 @@
             </div>
         {!! $products->links() !!}
     </div>
-
     </body>
+    @push('scripts')
+    <script>
+        function decrement(e) {
+            const btn = e.target.parentNode.parentElement.querySelector(
+                'button[data-action="decrement"]'
+            );
+            const target = btn.nextElementSibling;
+            let value = Number(target.value);
+            value--;
+            target.value = value;
+        }
+
+        function increment(e) {
+            const btn = e.target.parentNode.parentElement.querySelector(
+                'button[data-action="decrement"]'
+            );
+            const target = btn.nextElementSibling;
+            let value = Number(target.value);
+            value++;
+            target.value = value;
+        }
+
+        const decrementButtons = document.querySelectorAll(
+            `button[data-action="decrement"]`
+        );
+
+        const incrementButtons = document.querySelectorAll(
+            `button[data-action="increment"]`
+        );
+
+        decrementButtons.forEach(btn => {
+            btn.addEventListener("click", decrement);
+        });
+
+        incrementButtons.forEach(btn => {
+            btn.addEventListener("click", increment);
+        });
+    </script>
+
+    @endpush
+    
 </x-app-layout>
