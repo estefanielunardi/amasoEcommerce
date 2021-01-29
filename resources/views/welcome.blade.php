bab241b35c4dac59e0c79f4e99a367c1ba13abfb
Laura Bassani <bassanilaurarita@gmail.com>
2021-01-29 07:29:25 +0000

    Catalog: added JS for counter

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -56,7 +56,7 @@
                                         <button data-action="decrement" class="counter greenLightBg beigeLight h-full w-20 rounded-l-2xl cursor-pointer outline-none">
                                         <span class="m-auto text-2xl font-thin">
                                                 <a href="{{ route('removeProductCart' , $product->id) }}">-</a>
-                                            </span>
+                                        </span>
                                         </button>
                                         <input type="number" class="counter border-transparent outline-none focus:outline-none text-center w-12 greenAmasoBg font-semibold text-md   md:text-basecursor-default flex items-center text-white  outline-none" name="custom-input-number" value="0"></input>
                                         <button data-action="increment" class="counter  greenLightBg  beigeLight  h-full w-20 rounded-r-2xl cursor-pointer outline-none">
@@ -77,7 +77,46 @@
         </div>
         {!! $products->links() !!}
     </div>
+    @push('scripts')
+    <script>
+        function decrement(e) {
+            const btn = e.target.parentNode.parentElement.querySelector(
+                'button[data-action="decrement"]'
+            );
+            const target = btn.nextElementSibling;
+            let value = Number(target.value);
+            value--;
+            target.value = value;
+        }
 
+        function increment(e) {
+            const btn = e.target.parentNode.parentElement.querySelector(
+                'button[data-action="decrement"]'
+            );
+            const target = btn.nextElementSibling;
+            let value = Number(target.value);
+            value++;
+            target.value = value;
+        }
+
+        const decrementButtons = document.querySelectorAll(
+            `button[data-action="decrement"]`
+        );
+
+        const incrementButtons = document.querySelectorAll(
+            `button[data-action="increment"]`
+        );
+
+        decrementButtons.forEach(btn => {
+            btn.addEventListener("click", decrement);
+        });
+
+        incrementButtons.forEach(btn => {
+            btn.addEventListener("click", increment);
+        });
+    </script>
+
+    @endpush
     
 
 </x-app-layout>
