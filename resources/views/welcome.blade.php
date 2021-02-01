c315c3e784bc51ee11ea842afbdc7ce185f275cf
Carmenprz <carmenp.gomez99@gmail.com>
2021-02-01 15:40:14 +0100

    Catalog: product image with fixed size and scroll description

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,7 +29,7 @@
             @foreach($products as $product)
             <div class=" px-1 w-full flex flex-col p-6 sm:w-1/2 lg:w-1/3">
                 <section class="w-72 h-96 shadow-lg rounded-xl">
-                    <header class="relative">
+                    <header class="h-48 overflow-hidden">
                         <img alt="Placeholder" class="rounded-xl rounded-b-none object-fill w-full" src="{{ asset('storage') .'/'. $product->image}}">
                     </header>
 
@@ -38,7 +38,7 @@
                             <h2 class="productTitle">{{$product->name}}</h2>
                             <h3 class="productProductor">Productor: {{$product->artisans->name}}</h3>
                         </div>
-                        <div class="block py-1">
+                        <div class="block py-1 h-12 overflow-auto">
                             <p class="productDescription">
                                 {{$product->description}}
                             </p>
@@ -47,7 +47,7 @@
                             @if ($product->stock > $product->sold)
                             <p class="inline-block productPrice">{{number_format($product->price / 100,2)}} €</p>
                             <div class="grid justify-items-center">
-                                <p class="text-sm">Añadir al carrito:</p>
+                                <p class="text-xs">Añadir al carrito:</p>
                                 <div class="flex flex-row h-9 w-full rounded-lg relative bg-transparent mt-1 vollkorn">
                                     <form action="{{ route('removeProductCart' , $product->id) }}" method="POST">
                                         <button data-action="decrement" type="submit" class="counter greenLightBg beigeLight h-full w-9 rounded-l-2xl cursor-pointer outline-none">
