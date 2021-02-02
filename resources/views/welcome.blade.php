4950d0c4560b79615ace6f426f994f0b1312dcad
Carmenprz <carmenp.gomez99@gmail.com>
2021-02-02 19:17:47 +0100

    Landing: hero section title aligned to left

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,11 @@
 <x-app-layout>
     <header class="static">
         <img class="w-full h-64 object-cover lg:h-full" src="./image/cover2.jpg">
-        <section class="absolute top-24 left-10 w-52 lg:w-96 lg:top-52 lg:left-52">
-            <p class="heroTitle text-2xl pb-5 lg:text-5xl">
+        <section class="absolute top-24 left-10 w-52 lg:w-1/3 lg:top-52 lg:left-52">
+            <p class="heroTitle text-left text-2xl pb-5 lg:text-5xl">
                 Nuestros alimentos tienen orígenes cercanos a tí.
             </p>
-            <p class="heroText text-xs lg:text-xl">
+            <p class="heroText text-left text-xs lg:w-96 lg:text-xl">
                 La relación con nuestros proveedores está basada en la vertiente humana por encima de la comercial.
             </p>
         </section>
