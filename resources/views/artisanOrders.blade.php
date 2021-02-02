b74df132ab821f31824c703272e774d524563b05
Laura Bassani <bassanilaurarita@gmail.com>
2021-02-02 17:11:35 +0000

    Artisan Order: added blade

---
+++ resources/views/artisanOrders.blade.php
@@ -0,0 +1,57 @@
+<x-app-layout>
+    <h2 class="title p-10">Mis pedidos</h2>
+    <div class="flex  p-4" >
+        <table class="border-collapse w-full">
+            <thead>
+                <tr>
+                    <th class="p-3 font-bold vollkorn text-white greenLightBg hidden lg:table-cell">Producto</th>
+                    <th class="p-3 font-bold vollkorn text-white greenLightBg hidden lg:table-cell">Cantidad</th>
+                    <th class="p-3 font-bold vollkorn text-white greenLightBg hidden lg:table-cell">Total</th>
+                    <th class="p-3 font-bold vollkorn text-white greenLightBg hidden lg:table-cell">Comprador</th>
+                    <th class="p-3 font-bold vollkorn text-white greenLightBg hidden lg:table-cell">Email</th>
+                    <th class="p-3 font-bold vollkorn text-white greenLightBg hidden lg:table-cell">Direccion</th>
+                    <th class="p-3 font-bold vollkorn text-white greenLightBg hidden lg:table-cell">C.P</th>
+                    <th class="p-3 font-bold vollkorn text-white greenLightBg hidden lg:table-cell">Fecha</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="bg-white lg:hover:bg-gray-100 flex lg:table-row flex-row lg:flex-row flex-wrap lg:flex-no-wrap mb-10 lg:mb-0">
+                    <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
+                        <span >Mermelada de fresa</span>
+                        
+                    </td>
+                    <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
+                        <span>3</span>
+                        
+                    </td>
+                    <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
+                        <span >9 €</span>
+                        
+                    </td>
+                    <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
+                        <span >Maria Mercedes</span>
+                        
+                    </td>
+                    <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
+                        <span >maria@mercedes</span>
+                    
+                    </td>
+                    <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
+                        <span >Calle Melancolia nro 7</span>                    
+                    </td>
+                    <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
+                        <span >08014</span>
+                        
+                    </td>
+                    <td class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
+                        <span >20/12/2020</span>
+            
+                    </td>
+                </tr>
+                
+                
+            </tbody>
+        </table>
+
+    </div>
+</x-app-layout>
