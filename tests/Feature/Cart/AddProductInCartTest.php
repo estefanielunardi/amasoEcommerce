0f7a167a8e38e66d49ce507a1b748a30c61c49a9
Laura Bassani <bassanilaurarita@gmail.com>
2021-02-03 18:17:11 +0000

    Refactoring: added setup in cart tests

--- tests/Feature/Cart/AddProductInCartTest.php
+++ tests/Feature/Cart/AddProductInCartTest.php
@@ -14,36 +14,29 @@ class AddProductInCartTest extends TestCase
 
     use RefreshDatabase;
 
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testRouteAddProductCart()
-    {   
-        $this->withoutExceptionHandling();
-        $this->actingAs(User::factory()->create(['isArtisan'=>true, 'id'=>1]));        
-        Artisan::factory()->create(['user_id'=>1, 'id'=>1]);
-       
-        $product= Product::factory()->create(['image'=> null]);
+    private Product $product;
 
-        $response = $this->get(route('cartAddProduct', $product->id));
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->actingAs(User::factory()->create(['isArtisan' => true, 'id' => 1]));
+        Artisan::factory()->create(['user_id' => 1, 'id' => 1]);
+
+        $this->product = Product::factory()->create(['image' => null, 'name' => 'mermelada']);
+    }
+
+    public function testRouteAddProductCart()
+    {
+        $response = $this->get(route('cartAddProduct', $this->product->id));
 
         $response->assertRedirect('/');
     }
 
     public function testAddProductToCart()
     {
-        $this->withoutExceptionHandling();
-        $this->actingAs(User::factory()->create(['isArtisan'=>true, 'id'=>1]));        
-        Artisan::factory()->create(['user_id'=>1, 'id'=>1]);
-       
-        $product= Product::factory()->create(['image'=> null, 'name'=>'mermelada']);
-
-        $this->get(route('cartAddProduct', $product->id));
+        $this->get(route('cartAddProduct', $this->product->id));
 
         $this->assertDatabaseCount('product_user', 1)
-        ->assertDatabaseHas('product_user', ['product_id'=>1,'user_id'=>1, 'amount'=>1]);
+            ->assertDatabaseHas('product_user', ['product_id' => 1, 'user_id' => 1, 'amount' => 1]);
     }
-
 }
--- tests/Feature/Cart/RemoveProductFromCartTest.php
+++ tests/Feature/Cart/RemoveProductFromCartTest.php
@@ -11,49 +11,40 @@ use App\Models\Product;
 class RemoveProductFromCartTest extends TestCase
 {
     use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->actingAs(User::factory()->create(['isArtisan' => true, 'id' => 1]));
+        Artisan::factory()->create(['user_id' => 1, 'id' => 1]);
+        $this->product = Product::factory()->create(['image' => null]);
+    }
     public function testRouteDeleteProductFromCart()
     {
-        $this->withoutExceptionHandling();
-        $this->actingAs(User::factory()->create(['isArtisan'=>true, 'id'=>1]));        
-        Artisan::factory()->create(['user_id'=>1, 'id'=>1]);
-               
-        $product= Product::factory()->create(['image'=> null]);
-
-        $response = $this->delete(route('removeProductCart', $product->id));
+        $response = $this->delete(route('removeProductCart', $this->product->id));
 
         $response->assertRedirect();
     }
 
     public function testDecreaseAmountOfProductFromCartnDB()
     {
-        $this->withoutExceptionHandling();
-        $this->actingAs(User::factory()->create(['isArtisan'=>true, 'id'=>1]));        
-        Artisan::factory()->create(['user_id'=>1, 'id'=>1]);
-       
-        $product = Product::factory()->create(['image'=> null]);
-        $this->get(route('cartAddProduct', $product->id));
-        $this->get(route('cartAddProduct', $product->id));
-        
-        $this->delete(route('removeProductCart', $product->id));
+        $this->get(route('cartAddProduct', $this->product->id));
+        $this->get(route('cartAddProduct', $this->product->id));
+
+        $this->delete(route('removeProductCart', $this->product->id));
 
         $this->assertDatabaseCount('product_user', 1)
-        ->assertDatabaseHas('product_user', ['product_id'=>1,'user_id'=>1, 'amount'=>1]);
-
+            ->assertDatabaseHas('product_user', ['product_id' => 1, 'user_id' => 1, 'amount' => 1]);
     }
 
     public function testRemoveProductFromCartnDB()
     {
-        $this->withoutExceptionHandling();
-        $this->actingAs(User::factory()->create(['isArtisan'=>true, 'id'=>1]));        
-        Artisan::factory()->create(['user_id'=>1, 'id'=>1]);
-       
-        $product = Product::factory()->create(['image'=> null]);
-        $this->get(route('cartAddProduct', $product->id));
-        
-        $this->delete(route('removeProductCart', $product->id));
+        $this->get(route('cartAddProduct', $this->product->id));
+
+        $this->delete(route('removeProductCart', $this->product->id));
 
         $this->assertDatabaseCount('product_user', 0)
-        ->assertDatabaseMissing('product_user', ['product_id'=>1,'user_id'=>1]);
-
+            ->assertDatabaseMissing('product_user', ['product_id' => 1, 'user_id' => 1]);
     }
 }
