416846fa85f80bda1414340d6a7363af316cf644
Laura Bassani <bassanilaurarita@gmail.com>
2021-02-03 17:54:33 +0000

    Refactoring: added setup in payment tests

--- tests/Feature/Payment/PaymentConfirmationTest.php
+++ tests/Feature/Payment/PaymentConfirmationTest.php
@@ -7,20 +7,33 @@ use Tests\TestCase;
 use App\Models\User;
 use App\Models\Artisan;
 use App\Models\Product;
-use App\Mail\PurchaseConfirmation; 
+use App\Mail\PurchaseConfirmation;
 use Illuminate\Support\Facades\Mail;
 
 class PaymentConfirmationTest extends TestCase
 {
     use RefreshDatabase;
+
+    private User $user;
+    private user $alfredo;
+    private Artisan $artisan;
+    private Product $product2;
+    private Product $product1;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->user = User::factory()->create(['isArtisan' => true, 'id' => 1]);
+        $this->alfredo = User::factory()->create(['name' => 'Alfredo', 'email' => 'alfredo@alfredo', 'password' => '12345678']);
+        $this->artisan = Artisan::factory()->create(['user_id' => 1, 'id' => 1]);
+        $this->product1 = Product::factory()->create(['image' => null, 'id' => 1, 'name' => 'mermelada', 'stock' => 4]);
+        $this->product2 = Product::factory()->create(['image' => null, 'id' => 2, 'name' => 'pan', 'stock' => 4]);
+    }
+
     public function testRoute()
     {
-        $this->withoutExceptionHandling();
-        $this->actingAs(User::factory()->create(['isArtisan'=>true, 'id'=>1]));        
-        Artisan::factory()->create(['user_id'=>1, 'id'=>1]);       
-        $product= Product::factory()->create(['image'=> null, 'name'=>'mermelada']);
-
-        $this->get(route('cartAddProduct', $product->id));
+        $this->actingAs($this->user);
+        $this->get(route('cartAddProduct', $this->product1->id));
 
         $response = $this->put('/purchase');
 
@@ -29,85 +42,71 @@ class PaymentConfirmationTest extends TestCase
 
     public function testBuyProductsWhenPurchase()
     {
-        $this->actingAs(User::factory()->create(['isArtisan'=>true, 'id'=>1]));        
-        Artisan::factory()->create(['user_id'=>1, 'id'=>1]);       
-        $product= Product::factory()->create(['image'=> null, 'name'=>'mermelada']);
+        $this->actingAs($this->user);
+        $this->get(route('cartAddProduct', $this->product1->id));
 
-        $this->get(route('cartAddProduct', $product->id));
+        $this->put(route('purchase'));
 
-         $this->put(route('purchase'));
-
-        $this->assertDatabaseHas('product_user', ['buyed'=>1]);
+        $this->assertDatabaseHas('product_user', ['buyed' => 1]);
     }
 
     public function testDecreaseProductStockAfterPurchase()
     {
-        $this->withoutExceptionHandling();
+        $this->actingAs($this->user);
 
-        $this->actingAs(User::factory()->create(['isArtisan'=>true, 'id'=>1]));        
-        Artisan::factory()->create(['user_id'=>1, 'id'=>1]);       
-        $mermelada = Product::factory()->create(['image'=> null,'id'=>1, 'name'=>'mermelada', 'stock'=>4]);
-        $pan = Product::factory()->create(['image'=> null,'id'=>2, 'name'=>'pan', 'stock'=>4]);
-        $this->get(route('cartAddProduct', $mermelada->id));
-        $this->get(route('cartAddProduct', $mermelada->id));
-        $this->get(route('cartAddProduct', $pan->id));
-        $this->get(route('cartAddProduct', $pan->id));
-        
+        $this->get(route('cartAddProduct', $this->product1->id));
+        $this->get(route('cartAddProduct', $this->product1->id));
+        $this->get(route('cartAddProduct', $this->product2->id));
         $this->put('/purchase');
 
-        $this->assertDatabaseHas('products', ['id'=>1,'stock'=>2, 'name'=>'mermelada']);
-        $this->assertDatabaseHas('products', ['id'=>2,'stock'=>2, 'name'=>'pan']);
+        $this->assertDatabaseHas('products', ['id' => 1, 'stock' => 2, 'name' => 'mermelada']);
+        $this->assertDatabaseHas('products', ['id' => 2, 'stock' => 3, 'name' => 'pan']);
     }
 
     public function testSaveUserInformationCard()
     {
-        $this->withoutExceptionHandling();
-        
-        $this->actingAs(User::factory()->create(['name'=>'Alfredo', 'email'=>'alfredo@alfredo', 'password'=>'12345678']));
+        $this->actingAs($this->alfredo);
 
-        $card = [
-            'direction'=>'calle tomas',
-            'location'=>'Madrid',
-            'postal'=>2020,
-            'number_card'=>'SE12345678',
-            'expiring_date'=>'11/12/20',
+        $checkoutFormData = [
+            'direction' => 'calle tomas',
+            'location' => 'Madrid',
+            'postal' => 2020,
+            'number_card' => 'SE12345678',
+            'expiring_date' => '11/12/20',
         ];
 
-        $response = $this->put(route('purchase',$card));
+        $response = $this->put(route('purchase', $checkoutFormData));
 
         $response->assertRedirect('/');
 
-        $this->assertDatabaseHas('users',['name'=>'Alfredo', 
-                                        'email'=>'alfredo@alfredo', 
-                                        'password'=>'12345678',
-                                        'direction'=>'calle tomas',
-                                        'location'=>'Madrid',
-                                        'postal'=>2020,
-                                        'number_card'=>'SE12345678',
-                                        'expiring_date'=>'11/12/20']);
-    
+        $this->assertDatabaseHas('users', [
+            'name' => 'Alfredo',
+            'email' => 'alfredo@alfredo',
+            'password' => '12345678',
+            'direction' => 'calle tomas',
+            'location' => 'Madrid',
+            'postal' => 2020,
+            'number_card' => 'SE12345678',
+            'expiring_date' => '11/12/20'
+        ]);
     }
 
     public function testSendsEmailWhenPurchaseIsCompleted()
     {
-        $this->withoutExceptionHandling();
         Mail::fake();
-
-        $this->actingAs(User::factory()->create(['name'=>'Pepita','email'=>'pepi@ta','id'=>2,]));
+        $this->actingAs($this->user);
 
         $this->put('/purchase');
 
         Mail::assertSent(PurchaseConfirmation::class);
-
     }
 
-        public function testMailContent()
+    public function testMailContent()
     {
-        $user = User::factory()->create(['name'=>'Pepita','email'=>'pepi@ta','id'=>2,]);
-        $mailable = new PurchaseConfirmation($user->name);
+        $mailable = new PurchaseConfirmation($this->user->name);
 
         $mailable->assertSeeInHtml('Amasó');
-        $mailable->assertSeeInHtml($user->name);
+        $mailable->assertSeeInHtml($this->user->name);
         $mailable->assertSeeInHtml('tu pedido ha sido realizado con éxito');
         $mailable->assertSeeInHtml('Gracias por confiar en nosotros.');
     }
