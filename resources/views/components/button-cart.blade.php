308a018dcf30684a7852c9b9b001de45f92e5dbc
Joaquim-Frances <quimifrances@gmail.com>
2021-02-03 16:33:49 +0100

    ButtonCart:  Forever present

--- resources/views/components/button-cart.blade.php
+++ resources/views/components/button-cart.blade.php
@@ -1,4 +1,4 @@
-<div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+<div class="space-x-8 sm:-my-px sm:ml-10 sm:flex">
     <x-nav-link :href="route('cart')" :active="request()->routeIs('cart')">
         <div class="beigeAmasoBg rounded-full fixed bottom-20 right-10 z-40 shadow-2xl buttomDesktop buttomPhone">
             <svg class="beigeLight w-full text-center p-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
