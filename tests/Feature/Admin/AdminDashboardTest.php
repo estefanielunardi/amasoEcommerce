e648ab64a4bd46cd6b5c0daaf5498fa87f48b31d
Eni <estefanielunardi@users.noreply.github.com>
2021-02-04 12:01:52 +0100

    Admin: added view to admin carpet

--- tests/Feature/Admin/AdminDashboardTest.php
+++ tests/Feature/Admin/AdminDashboardTest.php
@@ -36,7 +36,7 @@ class AdminDashboardTest extends TestCase
         $response = $this->actingAs($this->admin)->get('/admin');
 
         $response->assertStatus(200)
-            ->assertViewIs('adminDashboard')
+            ->assertViewIs('admin.adminDashboard')
             ->assertViewHas('artisanList');
     }
 
