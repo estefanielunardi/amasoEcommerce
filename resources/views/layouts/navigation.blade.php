34affd14be59df98f97b856a4aaa968762860848
Joaquim-Frances <quimifrances@gmail.com>
2021-02-04 10:02:39 +0100

    NavBar: Adjusted Hamburguer size margin

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -99,7 +99,7 @@
         
         
         <!-- Hamburger -->
-        <div class="flex items-center md:hidden  m-20">
+        <div class="flex items-center md:hidden mr-14">
             <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md beigeLight hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
                 <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                     <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
