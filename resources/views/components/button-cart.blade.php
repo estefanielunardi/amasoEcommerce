d3a6de443d67fef0c78853b68662e5d68bbfa366
Quim <quimifrances@gmail.com>
2021-02-18 12:00:32 +0100

    Cart-Button: Hidden on movile

--- resources/views/components/button-cart.blade.php
+++ resources/views/components/button-cart.blade.php
@@ -1,4 +1,4 @@
-<div class="space-x-8 sm:-my-px sm:ml-10 sm:flex">
+<div class="space-x-8 sm:-my-px sm:ml-10 sm:flex md:hidden">
     <x-nav-link :href="route('cart')" :active="request()->routeIs('cart')">
         <div class="rounded-full w-20 h-20 flex items-center justify-center fixed bottom-20 right-10 z-40 shadow-2xl buttomDesktop buttomPhone || transition duration-500 ease-in-out beigeAmasoBg transform hover:-translate-y-1 hover:scale-110">
             <svg class="beigeLight w-16 h-16 text-center p-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
