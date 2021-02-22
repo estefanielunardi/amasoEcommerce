39dc99afd1b44586966cf22ffbc5400ca336dadb
Joaquim-Frances <quimifrances@gmail.com>
2021-02-22 11:19:03 +0100

    Cart Button Counter function working.

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -10,11 +10,20 @@ class CartController extends Controller
 {
     public function getProducts()
     {
+        $productsCounter = [];
         $id = auth()->id();
         $products = Cart::getProductsInBasket($id);
+        foreach ($products as $product) {
+            $product_id = $product->id;
+            $amountProduct = Cart::getProductAmount($product_id, $id);
+            array_push($productsCounter, $amountProduct);
+        }
+        
+        
+        $productsCount = array_sum($productsCounter);
         $total = Cart::calculateTotal($products);
 
-        return view('cart.cart', compact("products", "total"));
+        return view('cart.cart', compact("products", "total", "productsCount"));
     }
 
     public function addProduct($product_id)
