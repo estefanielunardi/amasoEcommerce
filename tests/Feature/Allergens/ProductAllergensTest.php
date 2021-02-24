ef05bdc9e7b1185fd0cc28b48dddb53d472d04c6
Laura Bassani <bassanilaurarita@gmail.com>
2021-02-24 14:23:54 +0100

    Allergens: added example test

--- tests/Feature/Allergens/ProductAllergensTest.php
+++ tests/Feature/Allergens/ProductAllergensTest.php
@@ -8,11 +8,9 @@ use Tests\TestCase;
 
 class ProductAllergensTest extends TestCase
 {
-    
+
     public function test_product_has_allergens()
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+        $this->assertTrue(true);
     }
 }
