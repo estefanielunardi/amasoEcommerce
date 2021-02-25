44ed9242fa4223e9ec1338ec3ee1933fa5792453
Carmenprz <carmenp.gomez99@gmail.com>
2021-02-25 11:21:25 +0100

    Artisans: anchor link in all card instead of only in picture

--- resources/views/artisan/artisans.blade.php
+++ resources/views/artisan/artisans.blade.php
@@ -9,17 +9,17 @@
 
     <div class="flex flex-wrap justify-center md:space-x-20 my-12 mx-auto px-4 md:px-12">
         @foreach ($artisans as $artisan)           
-        <div class="flex flex-col -mx-1 lg:-mx-4 pb-10 items-center">
-            <div class="relative w-56 h-56 overflow-hidden rounded-full">
-                <img alt="Placeholder" class="object-fill w-full rounded-2xl" src="{{$artisan->image}}"/>
-            </div>              
-            <a href="/artisan/{{$artisan->slug}}"> 
-                <p class="vollkorn text-xl pt-5">{{$artisan->name}}</p>
-            </a>
-            <p class="w-52 exo text-xs italic text-center pt-2">
-                {{$artisan->description}}
-            </p>
-        </div>
+        <a href="/artisan/{{$artisan->slug}}"> 
+            <div class="flex flex-col -mx-1 lg:-mx-4 pb-10 items-center">
+                <div class="relative w-56 h-56 overflow-hidden rounded-full">
+                    <img alt="Placeholder" class="object-fill w-full rounded-2xl" src="{{$artisan->image}}"/>
+                </div>              
+                    <p class="vollkorn text-xl pt-5">{{$artisan->name}}</p>
+                <p class="w-52 exo text-xs italic text-center pt-2">
+                    {{$artisan->description}}
+                </p>
+            </div>
+        </a>
         @endforeach
         {!! $artisans->links() !!}
     </div>
