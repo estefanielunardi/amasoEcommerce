77fde00d6e17da07d9704b00cfd76a8f7fd9fbb7
Laura Bassani <bassanilaurarita@gmail.com>
2021-02-28 13:02:04 +0100

    User Profile: added view

---
+++ resources/views/user/userProfile.blade.php
@@ -0,0 +1,15 @@
+<x-app-layout>
+    <div>
+        <h1 class="title pl-4 pb-10 pt-5 lg:pt-20">Bienvenido {{$user->name}}</h1>
+    </div>
+    <div>
+        <h1 class="title text-center pb-10 pt-5 lg:pt-20">Tu Historial de Compras</h1>
+    </div>
+    <article class="max-w-screen-xl pl-4 sm:pl-10 xl:pl-20 mx-auto px-4">
+        <div class=" ml-6 flex flex-wrap justify-center">
+            @foreach($userHistoryProducts as $product)
+            <x-product-card :product="$product" />
+            @endforeach
+        </div>
+    </article>
+</x-app-layout>
