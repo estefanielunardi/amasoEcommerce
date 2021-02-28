11d37e45a05b90079b8bb50a294517bd00361dfd
Laura Bassani <bassanilaurarita@gmail.com>
2021-02-28 13:01:53 +0100

    User profile: added Implementation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,14 +1,26 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
+use App\Models\Product;
+use Illuminate\Support\Facades\DB;
 use App\Models\User;
 
 class UserController extends Controller
 {
-    public function profile($id) {
-       
-        return view('user.userProfile');
+    public function profile() 
+    {
+        $id = auth()->id();
+        $user = User::find($id);
+        $userHistoryProducts = [];
+        $productIds = DB::table('product_user')
+                    ->where('user_id','=', $id)
+                    ->orderByDesc('updated_at')
+                    ->get('product_id');
+
+        foreach($productIds as $id)
+        {
+            array_push($userHistoryProducts, Product::find($id->product_id));
+        }      
+        return view('user.userProfile', compact('userHistoryProducts','user'));
     }
 }
--- tests/Feature/User/UserSeeProfileTest.php
+++ tests/Feature/User/UserSeeProfileTest.php
@@ -5,26 +5,63 @@ namespace Tests\Feature\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 use App\Models\User;
+use App\Models\Artisan;
+use App\Models\Product;
 
 class UserSeeProfileTest extends TestCase
 {
     use RefreshDatabase;
+
+    private User $user;
+    private user $alfredo;
+    private Artisan $artisan;
+    private Product $product2;
+    private Product $product1;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->user = User::factory()->create(['isArtisan' => true, 'id' => 1]);
+        $this->alfredo = User::factory()->create(['name' => 'Alfredo', 'email' => 'alfredo@alfredo', 'password' => '12345678']);
+        $this->artisan = Artisan::factory()->create(['user_id' => 1, 'id' => 1]);
+        $this->product1 = Product::factory()->create(['image' => null, 'id' => 1, 'name' => 'mermelada', 'stock' => 4]);
+        $this->product2 = Product::factory()->create(['image' => null, 'id' => 2, 'name' => 'pan', 'stock' => 4]);
+    }    
+    
     public function testUserRoute()
     {
         $this->withoutExceptionHandling();
         $this->actingAs(User::factory()->create());
-        $response = $this->get('/user/profile/1');
+        $response = $this->get('/user/profile');
 
         $response->assertStatus(200);
     }
 
     public function testReturnUserProfile()
     {
+
         $this->withoutExceptionHandling();
         $this->actingAs(User::factory()->create());
-        $response = $this->get('/user/profile/1');
+        $response = $this->get('/user/profile');
 
         $response->assertViewIs('user.userProfile');
+        
        
     }
+
+    public function testReturnUserProfileWithHistoryProducts()
+    {
+        $this->withoutExceptionHandling();
+        $this->actingAs(User::factory()->create(['id'=>4]));
+        $this->get(route('cartAddProduct', $this->product1->id));
+        $this->get(route('cartAddProduct', $this->product2->id));
+        $this->put('/purchase');
+    
+        $response = $this->get('/user/profile');
+
+        $response->assertViewIs('user.userProfile')
+        ->assertViewHas('userHistoryProducts')       
+        ->assertViewHas('user');       
+    }
+    
 }
