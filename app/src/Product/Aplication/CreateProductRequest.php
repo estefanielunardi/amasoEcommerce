01da94a6136d5e81ab7e56951b550e919d5f382f
Eni <estefanielunardi@users.noreply.github.com>
2021-03-02 17:34:38 +0100

    Aplication: create product request

--- app/src/Product/Aplication/CreateProductRequest.php
+++ app/src/Product/Aplication/CreateProductRequest.php
@@ -9,12 +9,23 @@ class CreateProductRequest
     private string $description;
     private int $price;
     private int $stock;
-    private int $sold;
     private int $artisan_id;
 
-    public function __construct(string $name, int $price, string $description, int $stock, int $sold, int $artisan_id)
-    {
+    public function __construct(string $name, int $price, string $description, int $stock, int $artisan_id)
+    {   
+        $this->name = $name;
+        $this->price = $price;
+        $this->description = $description;
+        $this->stock = $stock;
+        $this->artisan_id = $artisan_id;
         
     }
+
+    public function getName()
+    {
+        return $this->name;
+
+    }
+
     
 }
