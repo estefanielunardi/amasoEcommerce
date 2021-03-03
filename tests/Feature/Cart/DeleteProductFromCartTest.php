205e6cffae2bcc4d7d458e077ea2a9e0224b8488
Eni <estefanielunardi@users.noreply.github.com>
2021-03-03 12:14:46 +0100

    Cart: User can Not Delete From Cart a Product Previous Buyed

--- tests/Feature/Cart/DeleteProductFromCartTest.php
+++ tests/Feature/Cart/DeleteProductFromCartTest.php
@@ -41,4 +41,19 @@ class DeleteProductFromCartTest extends TestCase
         $this->assertDatabaseCount('product_user', 0)
             ->assertDatabaseMissing('product_user', ['product_id' => 1, 'user_id' => 1]);
     }
+
+    public function testNotDeleteProductPreviousBuyedFromCart()
+    {
+        $this->get(route('cartAddProduct', $this->product->id));
+
+        $this->put(route('purchase'));
+
+        $this->get(route('cartAddProduct', $this->product->id));
+       
+        $this->delete(route('deleteProductCart', $this->product->id));
+
+        $this->assertDatabaseCount('product_user', 1)
+            ->assertDatabaseMissing('product_user', ['product_id' => 1, 'user_id' => 1, 'id'=>2])
+            ->assertDatabaseHas('product_user', ['product_id' => 1, 'user_id' => 1 , 'id'=>1 , 'buyed'=>true]);
+    }
 }
