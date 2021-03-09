37907f2228d5bb16526c8cca237a4b2072cecc2a
Laura Bassani <bassanilaurarita@gmail.com>
2021-03-09 11:02:59 +0100

    Refactoring: added info component in product page

--- resources/views/products/productPage.blade.php
+++ resources/views/products/productPage.blade.php
@@ -1,47 +1,20 @@
 <x-app-layout>
     <section class="block pt-16 md:ml-20 ml-10 mr-10 ">
             <section class=" flex flex-col md:flex-row">
-                <article class="flex justify-start pb-6">
-                    <div class="max-h-96 w-96 overflow-hidden rounded-xl shadow-lg">
-                        <img class="object-fill w-full" src="{{$product->image}}" alt="foto de perfil">
-                    </div>
-                </article>
-                <article class=" md:pl-10">
-                    <div>
-                        <h2 class="block title text-4xl	"> {{$product->name}} </h2>
-                        <h2 class="productProductor">Productor: 
-                            <a href="/artisan/{{$product->artisans->slug}}">{{$product->artisans->name}}</a>
-                        </h2>
-                    </div>
-                    <div class="block py-2 flex items-center ">
-                        @if ($product->stock > $product->sold)
-                        <p class=" pr-2 text-3xl inline-block productPrice">{{number_format($product->price / 100,2)}} €</p>
-                        <div class="grid justify-items-center">
-                            
-                            <div class="flex flex-row ml-2 h-9 w-full justify-center rounded-lg relative bg-transparent mt-1 vollkorn">                      
-                                <button class="greenLightBg  rounded-xl">
-                                    <span class="m-auto text-lg p-2 font-thin exo text-white">
-                                        <a href="{{ route('cartAddProduct' , $product->id) }}">Añadir</a>
-                                    </span>
-                                </button>
-                            </div>
-                        </div>
-                        @else
-                        <p class="text-lg beigeAmasoBg p-1 mt-1 leading-4">Producto agotado</p>
-                        @endif
-                    </div>
+               <article class="flex justify-start pb-6">
+                    <x-product.page.info :product=$product/>
                     <div>
                         <h2 class="block text-md greenAmaso mt-1 mb-2">Valoración de los usuarios:</h2>
                         @if (isset($midRate))
                         <div class="flex flex-row justify-start items-baseline mb-2">
                             <div class="flex flex-row items-baseline">
                             @for ($i = 1; $i <= $midRate; $i++)
-                            <img src="{{URL::to('/image/star-solid.svg')}}" alt="star-solid" width="25" class="mr-4">
+                                <img src="{{URL::to('/image/star-solid.svg')}}" alt="star-solid" width="25" class="mr-4">
                             @endfor
                             @for ($i = 1; $i <= $emptyStars; $i++)
                                 <img src="{{URL::to('/image/star-regular.svg')}}" alt="star-empty" width="25" class="mr-4">
                             @endfor
-                        </div>
+                            </div>
                         </div>
                         <p class="italics eko text-sm greenAmaso mb-4">( {{$votesCount}} valoraciones )</p>
                         @else
