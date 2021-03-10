413785e1159d2ef520a3c0dca74bc4ba6cc71641
Carmenprz <carmenp.gomez99@gmail.com>
2021-03-10 11:37:18 +0100

    Components: product card component deleted

--- resources/views/products/searchedProduct.blade.php
+++ resources/views/products/searchedProduct.blade.php
@@ -1,10 +1,12 @@
 <x-app-layout>
     <h1 class="title text-center pb-10 pt-5 lg:pt-20">Resultado encontrado por {{$name}}</h1>
-    <article class="max-w-screen-xl pl-4 sm:pl-10 xl:pl-20 mx-auto px-4">
-        <div class=" ml-6 flex flex-wrap justify-center">
-            @foreach($products as $product)
-            <x-product-card :product="$product" />
-            @endforeach
-        </div>
-    </article>
+
+    <div class="w-full flex flex-wrap justify-center content-start p-6 xl:px-32 2xl:px-56">
+        @foreach($products as $product)
+        <x-product.card.product-card :product="$product" :artisan=null :highlightProducts=null :bestSellers=null/>
+        @endforeach
+    </div>
+    <div class="px-6 pb-20 xl:px-32 2xl:px-56">
+        {!! $products->links() !!}
+    </div>
 </x-app-layout>
