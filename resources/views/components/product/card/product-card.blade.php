819409edde647671f076cf7ce8291ac0ba972d3e
Carmenprz <carmenp.gomez99@gmail.com>
2021-03-11 09:50:57 +0100

    Product card: added anchor for see product view

--- resources/views/components/product/card/product-card.blade.php
+++ resources/views/components/product/card/product-card.blade.php
@@ -7,6 +7,8 @@
 @empty($highlightProducts || $bestSellers)
 <section class="w-72 h-80 m-5 shadow-lg rounded-xl transform duration-500 ease-in-out hover:scale-105">
 @endempty
-    <x-product.card.header :product="$product" />
-    <x-product.card.body :product="$product" :artisan="$artisan"/>
+    <a href="/product/{{$product->id}}">
+        <x-product.card.header :product="$product" />
+        <x-product.card.body :product="$product" :artisan="$artisan"/>
+    </a>
 </section>
