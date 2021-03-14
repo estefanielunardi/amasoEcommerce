50803a7961b90c5f0c595ab5533b35d83c0cb3ca
Laura Bassani <bassanilaurarita@gmail.com>
2021-03-14 12:23:01 +0100

    Refactoring: added repository  service provider

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use App\Repositories\Cart\CartRepository;
+use App\Repositories\Cart\ICartRepository;
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->app->bind(ICartRepository::class, CartRepository::class);
+    }
+}
