7b6f1a3a5e8431e38f94c49799d654a8b6ab6bb2
Eni <estefanielunardi@users.noreply.github.com>
2021-03-16 09:12:40 +0100

    Test push welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -53,6 +53,7 @@
         {!! $products->links() !!}
     </div>
 
+
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/assets/css/chat.min.css">
     <script>
         var botmanWidget = {
