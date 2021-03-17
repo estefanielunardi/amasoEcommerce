672ae77d16f6c467c7e0416cf3fba9dd8f837194
Eni <estefanielunardi@users.noreply.github.com>
2021-03-17 12:31:56 +0100

    Searched: Added search bar into the new view of product searched

--- resources/views/products/searchedProduct.blade.php
+++ resources/views/products/searchedProduct.blade.php
@@ -1,6 +1,7 @@
 <x-app-layout>
     <h1 class="title text-center pb-10 pt-5 lg:pt-20">Resultado encontrado por {{$name}}</h1>
-
+    <x-welcome.searcher />
+    
     <div class="w-full flex flex-wrap justify-center content-start p-6 xl:px-32 2xl:px-56">
         @foreach($products as $product)
         <x-product.card.product-card :product="$product" :artisan=null :highlightProducts=null :bestSellers=null/>
